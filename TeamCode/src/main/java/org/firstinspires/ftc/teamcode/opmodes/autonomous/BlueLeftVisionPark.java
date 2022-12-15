29b0ea4d75be499a664c0e38e082fdb1c6e8c443
ma1ma1 <emolga12345678910@gmail.com>
2022-12-14 22:04:07 -0500

    yaea?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/BlueLeftVisionPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/BlueLeftVisionPark.java
@@ -10,7 +10,7 @@ public class BlueLeftVisionPark {
     // testing if git is working now???
     private RobotMain robot;
     private DriveTrain driveTrain;
-    private ScoringMechanism scoringMechanismmm;
+    private ScoringMechanism scoringMechanism;
     private ElapsedTime timer = new ElapsedTime();
 
 }
