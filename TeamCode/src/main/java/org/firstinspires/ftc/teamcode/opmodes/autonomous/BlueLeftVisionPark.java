aeba16700946a6de207bdfe42c51eabb943d407d
KasinadhuniProgrammer <rachitipidudraksha@gmail.com>
2022-12-14 21:55:51 -0500

    whos doing this

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/BlueLeftVisionPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/BlueLeftVisionPark.java
@@ -1,4 +1,16 @@
 package org.firstinspires.ftc.teamcode.opmodes.autonomous;
 
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.RobotMain;
+import org.firstinspires.ftc.teamcode.subsystems.DriveTrain;
+import org.firstinspires.ftc.teamcode.subsystems.ScoringMechanism;
+
 public class BlueLeftVisionPark {
+    // testing if git is working now???
+    private RobotMain robot;
+    private DriveTrain driveTrain;
+    private ScoringMechanism scoringMechanism;
+    private ElapsedTime timer = new ElapsedTime();
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/RedRightVisionPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/RedRightVisionPark.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.opmodes.autonomous;
 
 public class RedRightVisionPark {
+
 }
